f3b78a3eaf32b035bd66fe6fe56efd4191d2e637
Amir Sharapov <amirfounder18@gmail.com>
2021-08-14 22:16:57 -0500

    built package with logger class

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ setup(
   name="activity-logger",
   version="0.0.1",
   description="Logs activity to a file",
-  py_modules=["main"],
+  py_modules=[
+    "main",
+    "utils.Logger"
+  ],
   package_dir={
     "":"src"
   }
